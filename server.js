ca80cf612b0e989e304f4767b9bf51af8fc64007
YJyamashita <yuuji.yamashita.yg@east.ntt.co.jp>
2022-03-18 23:52:46 +0900

    35

---
+++ server.js
@@ -0,0 +1,13 @@
+const socketio = require('socket.io');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '')));
+
+server = app.listen(3000);
+
+const io = require("socket.io")(server);
+
+const PORT = 3000 || process.env.PORT;
+console.log(`Server running on port ${PORT}`);
