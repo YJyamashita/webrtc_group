c2922a5012b2b2de726d409481745f3c78794649
YJyamashita <yuuji.yamashita.yg@east.ntt.co.jp>
2022-03-19 22:11:41 +0900

    38

--- server.js
+++ server.js
@@ -11,3 +11,25 @@ const io = require("socket.io")(server);
 
 const PORT = 3000 || process.env.PORT;
 console.log(`Server running on port ${PORT}`);
+var userConnection = [];
+
+io.on('connection', (socket) => {
+	socket.on('users_info_to_signaling_server', (data) => {
+		var other_users = userConnection.filter(p => p.meeting_id == data.meetingid);
+		userConnection.push({
+			connectionId: socket.id,
+			user_id: data.current_user_name,
+			meeting_id: data.meetingid
+		})
+		console.log(`all users: ${userConnection.map(a => a.connectionId)}`);
+		console.log(`other users: ${other_users.map(a => a.connectionId)}`);
+
+		other_users.forEach(v => {
+			socket.to(v.connectionId).emit('other_users_to_inform', {
+				other_user_id: data.current_user_name,
+				connId: socket.id
+			})
+		});
+		socket.emit('newConnectionInformation', other_users);
+	})
+})
